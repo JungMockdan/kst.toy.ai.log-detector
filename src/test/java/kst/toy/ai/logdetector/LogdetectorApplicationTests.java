ac4fb956f6017d895c58bd100da30d4627202723
JMD <gogo.dev.work@gmail.com>
2026-03-20 10:54:13 +0900

    initial

---
+++ src/test/java/kst/toy/ai/logdetector/LogdetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package kst.toy.ai.logdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogdetectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
